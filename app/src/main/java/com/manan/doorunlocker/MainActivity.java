3504546f9be50c90dd047cd73159cce5ff5bce1d
Manan <thedigitalmanan@gmail.com>
2021-01-13 11:56:04 +0530

    added aws functionality works outside of local network now

--- app/src/main/java/com/manan/doorunlocker/MainActivity.java
+++ app/src/main/java/com/manan/doorunlocker/MainActivity.java
@@ -2,14 +2,24 @@ package com.manan.doorunlocker;
 import android.annotation.SuppressLint;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -20,6 +30,15 @@ import java.nio.ByteOrder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.CognitoCachingCredentialsProvider;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CopyObjectRequest;
 
 import org.w3c.dom.Text;
 
@@ -32,13 +51,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setContentView(R.layout.activity_main);
         Button btnConnect = findViewById(R.id.gateButton);
         statusTV = findViewById(R.id.StatusTV);
         btnConnect.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SERVER_IP = "2405:204:340b:3a26:501f:a126:ef32:565d";
+                upload();
+                //SERVER_IP = "192.168.1.111";
+                SERVER_IP = "172.27.242.120";
+                        // SERVER_IP = "2405:204:340b:3a26:501f:a126:ef32:565d";
                 SERVER_IP = SERVER_IP.trim();
                 String SERVER_PORTs = "63890";
                 SERVER_PORT = Integer.parseInt(SERVER_PORTs.trim());
@@ -47,6 +72,31 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    public void upload() {
+        SendfeedbackJob job = new SendfeedbackJob();
+        String[] J = null;
+        job.doInBackground(J);
+    }
+    private class SendfeedbackJob extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String[] params) {
+            BasicAWSCredentials awsCreds = new BasicAWSCredentials("AKIAYCG5WKDQAVEQLYMD", "EfPD9jZ9JHWoOwQsvVIxYV6zvu8hxKUEenRlivI8");
+
+            AmazonS3 s3 = new AmazonS3Client(awsCreds);
+            s3.deleteObject("dooropener", "example2.txt");
+            CopyObjectRequest copyObjRequest = new CopyObjectRequest("dooropener", "example.txt", "dooropener", "example2.txt");
+            s3.copyObject(copyObjRequest);
+            statusTV.setText("Open");
+            return "some message";
+        }
+
+        @Override
+        protected void onPostExecute(String message) {
+            //process message
+        }
+    }
+
     private PrintWriter output;
     private BufferedReader input;
     class Thread1 implements Runnable {
